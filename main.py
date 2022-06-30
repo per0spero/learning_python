88265246d3ad1b5e5ddb815c9f281232cf3c7815
Ayana Seisekulova <98754791+per0spero@users.noreply.github.com>
2022-06-30 17:30:28 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Here I will learn to use classes and objects via building a multiple choice quiz
+
+from Class import Question
+
+questions = ["1. Question1 \na)haha \nb)huh \nc)uhd\n",
+             "2. Question2 \na)haha \nb)huh \nc)uhd\n",
+             "3. Question3 \na)haha \nb)huh \nc)uhd\n"
+             ]
+
+
+quiz = [Question(questions[0], "a"),
+        Question(questions[1], "b"),
+        Question(questions[2], "c"),
+        ]
+
+def run_quiz(quiz):
+    score = 0
+    for question in quiz:
+        prompt = input(question.prompt)
+        if prompt == question.answer:
+            score += 1
+    print('You got ' + str(score) + '/' + str(len(quiz)) + ' correct!')
+
+
+run_quiz(quiz)
